7a6bf6a1a301eade83ff329475a231ceeb41c33a
JeongMin Choi <jmchoi1225@ajou.ac.kr>
2020-01-13 13:17:58 +0900

    show html with server.js

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  var q = url.parse(req.url, true);
+  var filename;
+  if(q.pathname == '/'){
+    filename = "views/index.html";
+  }else{
+    filename = "views/" + q.pathname + ".html";
+  }
+  fs.readFile(filename, function(err, data) {
+    if(err){
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 NOT FOUND");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 }).listen(8080);

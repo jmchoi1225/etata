630bed0035fdcb3aff49cd0fb29ac9797b9d974d
JeongMin Choi <jmchoi1234@hotmail.com>
2020-01-17 13:30:03 +0900

    add group via js

--- views/registration.js
+++ views/registration.js
@@ -1,16 +1,66 @@
-/*const num_of_groups;
-const courses = new Array(num_of_groups);
-for(let i =0; i<num_of_groups; i++){
-    var node = document.createElement("div");
-    node.className = "group";
-    courses[i] = new Array(3);
-    // get the number of courses that has three/two/one star
-    const num_of_courses = new Array(3);
-    for(let j =0; j<3; j++){
-        //num_of_courses = num of courses that has 3-j stars
-        courses[i][j] = new Array(num_of_courses[j]);
-        for(let k = 0; k<num_of_courses[j]; k++){
-            //save the course in courses[i][j][k] 
-        }
+class course{
+    constructor(id){
+        this.id = id;
     }
-}*/
+    getId(){
+        return this.id;
+    }
+}
+class group{
+    constructor(name, firstCrs, otherCrs){
+        this.name = name;
+        this.firstCrs = firstCrs;
+        this.otherCrs = otherCrs;
+    }
+    getName(){
+        return this.name;
+    }
+    getFirstCrs(){
+        return this.firstCrs;
+    }
+    getOtherCrs(){
+        return this.otherCrs;
+    }
+}
+
+const groups = new Array();
+const first = new course("F028");
+const other = new Array(3);
+for(var i =0; i<3; i++){
+    other[i] = new Array();
+}
+other[0].push(new course("F028"));
+other[1].push(new course("F129"));
+groups.push(new group("테스트", first, other));
+
+
+for(let i =0; i<groups.length; i++){
+    const curGroup = groups[i];
+    var insertNode =  
+        '<div class = "group">' +
+            '<div class = "first">' +
+                '<div class = "groupContainer">' +curGroup.getName()+'</div>' +
+                '<hr>'+
+                '<div class = "groupContainer">'+
+                    '<button>'+curGroup.getFirstCrs().getId()+'</button>'+
+                '</div>'+
+                '<div class = "groupContainer">'+
+                    '<button>성공</button>'+
+                    '<button>실패</button>'+
+                '</div>'+
+            '</div>'+
+            '<div class = "others">';
+                
+    for(let j = 0; j<3; j++){
+        insertNode +='<div class = "groupContainer">';
+        for(let k = 0; k< curGroup.getOtherCrs()[j].length; k++){
+            insertNode +='<button>' +curGroup.getOtherCrs()[j][k].getId()+'</button>';
+        }
+        insertNode += '</div>';
+    }
+    insertNode += 
+            '</div>' +
+        '</div>';
+            
+    document.getElementById('content').innerHTML += insertNode;
+}

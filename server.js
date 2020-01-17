94f314b1b1f4653c605a2cff925dd492e8e52134
JeongMin Choi <jmchoi1234@hotmail.com>
2020-01-17 13:32:23 +0900

    server.js bug fixed

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   if(req.url === "/"){
     fs.readFile("./views/index.html", "UTF-8", function(err, data){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(data);
-    });
-  }else if(req.url === "/registration"){
-    fs.readFile("./views/registration.html", "UTF-8", function(err, data){
+      if(err){
+        throw err;
+      }
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(data);
-  });
-
+    });
   }else if(req.url.match("\.css$")){
       var cssPath = "./views/" + req.url;
       var fileStream = fs.createReadStream(cssPath, "UTF-8");
@@ -26,8 +23,11 @@ http.createServer(function (req, res) {
       res.writeHead(200, {"Content-Type": "text/javascript"});
       fileStream.pipe(res);
   }else{
-    var filename = "views/" + req.url;
+    var filename = "./views/" + req.url + ".html";
     fs.readFile(filename, function(err, data) {
+      if(err){
+        throw err;
+      }
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       res.end();

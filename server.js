81516e380790fc739fbb31194b2414aeebd3fff6
JeongMin Choi <jmchoi1234@hotmail.com>
2020-02-03 14:44:45 +0900

    server.js html 접근 방식 바꿈

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var mysql = require('mysql');
 
 http.createServer(function (req, res) {
-  if(req.url === "/"){
-    fs.readFile("./views/index.html", "UTF-8", function(err, data){
+  if(req.url.match("\.html$")){
+    var htmlPath = "./views/" + req.url;
+    fs.readFile(htmlPath, "UTF-8", function(err, data){
       if(err){
         throw err;
       }
       res.writeHead(200, {"Content-Type": "text/html"});
       res.end(data);
     });
-  }else if(req.url === "/registration"){
-    fs.readFile("./views/registration.html", "UTF-8", function(err, data){
-      if(err){
-        throw err;
-      }
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.end(data);
-  });
-
   }else if(req.url.match("\.css$")){
       var cssPath = "./views/" + req.url;
       var fileStream = fs.createReadStream(cssPath, "UTF-8");
       res.writeHead(200, {"Content-Type": "text/css"});
       fileStream.pipe(res);
-
   }else if(req.url.match("\.js$")){
       var jsPath = "./views/" + req.url;
       var fileStream = fs.createReadStream(jsPath);
